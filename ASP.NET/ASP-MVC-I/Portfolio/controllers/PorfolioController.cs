2dc05daef6b4f4ffb508ea321ebdfc67dafaec2c
Helmi Touati <touatizh@gmail.com>
2024-01-10 19:19:08 +0100

    Assignment `Portfolio I`

---
+++ ASP.NET/ASP-MVC-I/Portfolio/controllers/PorfolioController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+namespace Portfolio.Controllers;
+
+public class PortfolioController : Controller
+{
+    [HttpGet("")]
+    public string Index()
+    {
+        return "This is my index!";
+    }
+
+    [HttpGet("/projects")]
+    public string Projects()
+    {
+        return "These are my projects!";
+    }
+
+    [HttpGet("/contact")]
+    public string Contact()
+    {
+        return "This is my contact!";
+    }
+}
